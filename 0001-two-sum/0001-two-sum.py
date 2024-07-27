05f1ec76112093a4941b12a06a2de30ab16781a1
Pitika05 <143535539+Pitika05@users.noreply.github.com>
2024-07-27 15:29:55 +0530

    Time: 1870 ms (11.45%), Space: 17.3 MB (89.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,6 +3,6 @@ class Solution:
         for i in range(len(nums)-1):
             for j in range(i+1,len(nums)):
                 if i!=j and nums[i]+nums[j]==target:
-                    return (i,j)
+                    return [i,j]
         return []
         

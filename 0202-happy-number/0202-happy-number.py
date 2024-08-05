34f3861803cb65e14849254d427b3fd31b5773f7
Pitika05 <143535539+Pitika05@users.noreply.github.com>
2024-08-05 16:42:01 +0530

    Time: 45 ms (11.03%), Space: 16.4 MB (62.28%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,17 +1,18 @@
+
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def sumOfSquares(n):
-            num=n
-            total=0
-            while num>0:
-                digit=num%10
-                total+=digit**2
-                num=num//10
+        def sumOfSquares(num: int) -> int:
+            total = 0
+            while num > 0:
+                digit = num % 10
+                total += digit * digit
+                num //= 10
             return total
-        
-        for i in range(100):
-            n= sumOfSquares(n)
-            if n==1:
-                return True
-        else:
-            return False
+
+        s = set()  # Set to keep track of numbers we have encountered
+        while n != 1 and n not in s:
+            s.add(n)  # Add the current number to the set
+            n = sumOfSquares(n)  # Compute the next number in the sequence
+
+        return n == 1
+       
